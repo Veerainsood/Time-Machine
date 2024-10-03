febc4edcc1d82b59ff7140408c75feb99b304ab3
veerain <veerainsood1@gmail.com>
2024-10-04 00:11:50 +0530

    added forest cover

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { Sky } from '@react-three/drei'
-import './App.css'
-import {Canvas} from '@react-three/fiber'
-import { Physics } from '@react-three/cannon'
-import {Ground} from './Components/Ground'
-import { Player } from './Components/Player'
-import { FPV } from './Components/FPV'
-import { TreeModel } from './models/TreeModel'
-import { CharModel } from './models/Character'
-function App() {  
+import { Sky } from '@react-three/drei';
+import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Physics } from '@react-three/cannon';
+import {Ground} from './Components/Ground'; // Ensure this is the correct import for your Ground component
+import { Player } from './Components/Player';
+import { FPV } from './Components/FPV';
+import { TreeModel } from './models/TreeModel';
+import { CharModel } from './models/Character';
+import {Model} from './models/forestLandScape'; // Adjusted import statement
 
+function App() {  
   return (
     <>
-    <Canvas shadows>
-    <Sky sunPosition={[100,100,20]}/>
-    <ambientLight />
-    <FPV />
-    <Physics gravity={[0, -100, 0]}>
-      <Player/>
-      <TreeModel position={[1, 0, 3]} rotation={[0, Math.PI / 2, 0]} scale={[.5, .5, .5]}/>
-      <TreeModel position={[5, 0, 19]} rotation={[0, Math.PI / 2, 0]} scale={[.5, .5, .5]}/>
-      <TreeModel position={[4, 0, 12]} rotation={[0, Math.PI / 2, 0]} scale={[.5, .5, .5]}/>
-      <TreeModel position={[20, 0, 16]} rotation={[0, Math.PI / 2, 0]} scale={[.5, .5, .5]}/>
-      <TreeModel position={[7, 0, 9]} rotation={[0, Math.PI / 2, 0]} scale={[.5, .5, .5]}/>
-      <CharModel position={[10, 0, 1]} rotation={[0, Math.PI / 2, 0]} scale={[.014, .014, .014]}/>
-      <CharModel position={[23, 0, 4]} rotation={[0, Math.PI / 2, 0]} scale={[.014, .014, .014]}/>
-      <Ground/>
-    </Physics>
-    </Canvas>
-    <div className='absolute centered cursor'>+</div>
+      <Canvas shadows>
+        <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight />
+        <FPV />
+        <Physics gravity={[0, -100, 0]}>
+          <Player />
+          <TreeModel position={[1, 0, 3]} rotation={[0, Math.PI / 2, 0]} scale={[0.5, 0.5, 0.5]} />
+          <TreeModel position={[5, 0, 19]} rotation={[0, Math.PI / 2, 0]} scale={[0.5, 0.5, 0.5]} />
+          <TreeModel position={[4, 0, 12]} rotation={[0, Math.PI / 2, 0]} scale={[0.5, 0.5, 0.5]} />
+          <TreeModel position={[20, 0, 16]} rotation={[0, Math.PI / 2, 0]} scale={[0.5, 0.5, 0.5]} />
+          <TreeModel position={[7, 0, 9]} rotation={[0, Math.PI / 2, 0]} scale={[0.5, 0.5, 0.5]} />
+          <CharModel position={[10, 0, 1]} rotation={[0, Math.PI / 2, 0]} scale={[0.014, 0.014, 0.014]} />
+          <CharModel position={[23, 0, 4]} rotation={[0, Math.PI / 2, 0]} scale={[0.014, 0.014, 0.014]} />
+          
+          {/* Add the Forest model */}
+          <Model position={[0, -.6, 0]} scale={[10, 10, 10]}/>
+          {/* Uncomment the Ground component if needed */}
+          <Ground />
+        </Physics>
+      </Canvas>
+      <div className='absolute centered cursor'>+</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/models/forestLandScape.jsx
+++ src/models/forestLandScape.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import { useBox } from '@react-three/cannon';
 import { useGLTF } from '@react-three/drei';
+import React from 'react';
 
-export function ForestLandScape(props) {
-  const { nodes, materials } = useGLTF('/forest model.glb');
+export function Model(props) {
+  const { nodes, materials } = useGLTF('/forest/scene.gltf');
+
+  // Create a rigid body for the model
+  const [ref] = useBox(() => ({
+    mass: 0, // Set mass to 0 for static object
+    position: props.position || [0, 0, 0], // Use props for dynamic positioning
+    rotation: props.rotation || [0, 0, 0], // Use props for rotation
+    args: [1, 1, 1], // Adjust these values according to your model's dimensions
+  }));
+
   return (
-    <group {...props} dispose={null} position={[0, 0, 0]}> {/* Position the entire group at the bottom */}
-      <mesh
-        geometry={nodes.Plane.geometry}
-        material={materials['Forest ground']}
-        position={[0, 0, 0]} 
-      />
-      <mesh
-        geometry={nodes.Cube.geometry}
-        material={nodes.Cube.material}
-        position={[0, 1, 0]} 
-        rotation={[0, Math.PI / 9, 0]}
-        scale={[0.35, 1, 0.35]}
-      />
+    <group  {...props} dispose={null}>
+      <group rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh ref={ref} geometry={nodes.Object_2.geometry} material={materials.Floor} />
+        <mesh ref={ref} geometry={nodes.Object_3.geometry} material={materials.Green_Elka} />
+        <mesh ref={ref} geometry={nodes.Object_4.geometry} material={materials.Green_Elka} />
+        <mesh ref={ref} geometry={nodes.Object_5.geometry} material={materials.Green_Elka} />
+        <mesh ref={ref} geometry={nodes.Object_6.geometry} material={materials.Green_Elka} />
+        <mesh ref={ref} geometry={nodes.Object_7.geometry} material={materials.Green_Elka} />
+        <mesh ref={ref} geometry={nodes.Object_8.geometry} material={materials['Green_sosna.001']} />
+        <mesh ref={ref} geometry={nodes.Object_9.geometry} material={materials['Green_sosna.001']} />
+        <mesh ref={ref} geometry={nodes.Object_10.geometry} material={materials.Kust_1} />
+        <mesh ref={ref} geometry={nodes.Object_11.geometry} material={materials.Kust_2} />
+        <mesh ref={ref} geometry={nodes.Object_12.geometry} material={materials.Paporotnik} />
+        <mesh ref={ref} geometry={nodes.Object_13.geometry} material={materials['Wood_tree.001']} />
+        <mesh ref={ref} geometry={nodes.Object_14.geometry} material={materials['Wood_tree.002']} />
+      </group>
     </group>
   );
 }
 
-useGLTF.preload('/forest model.glb');
+useGLTF.preload('/forest/scene.gltf');
