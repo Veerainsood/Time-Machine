c68e3e63a82580ea327fbb813c744fe2d36cf1ec
aman <cs22b054@iittp.ac.in>
2024-10-05 09:19:43 +0000

    Added Player + Different Models + Movement of Player + Thrid person controller

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Sky, Stars } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Physics } from '@react-three/cannon';
 import {Ground} from './Components/Ground'; // Ensure this is the correct import for your Ground component
-import { Player } from './Components/Player';
+import { MyPlayer } from './Components/MyPlayer';
 import { FPV } from './Components/FPV';
 import { TreeModel } from './models/TreeModel';
 import { CharModel } from './models/Character';
@@ -10,6 +10,7 @@ import { TimeMachine } from './models/TimeMachine';
 import { ForestGen } from './Components/ForestGenerator';
 import { Tree2Model } from './models/Tree2';
 import { SkyCycle } from './Components/ChangeDayNight';
+import { Axes } from './Components/AxesHelper';
 function App() {  
   return (
     <>
@@ -17,13 +18,15 @@ function App() {
         <SkyCycle />
         <ambientLight /> 
         <FPV />
+        <Axes/>
         <Physics gravity={[0, -300, 0]}>
-          <Player />
+          <MyPlayer />
           <CharModel position={[10, 0, 1]} rotation={[0, Math.PI / 2, 0]} scale={[0.014, 0.014, 0.014]} />
           <CharModel position={[23, 0, 4]} rotation={[0, Math.PI / 2, 0]} scale={[0.014, 0.014, 0.014]} />
           <TimeMachine position={[40, 2,9]} rotation={[0, Math.PI/2, 0]} scale={[3, 3, 3]}/>
            {/* <Tree2Model position={[20, 2,9]} rotation={[0, Math.PI/2, 0]} scale={[1, 1, 1]}/> */}
            <ForestGen />
+            
           <Ground />
         </Physics>
       </Canvas>
