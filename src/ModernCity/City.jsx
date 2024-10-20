28cbf8096b3bafb2867a991a6ef2146faf04cb26
aman <cs22b054@iittp.ac.in>
2024-10-20 12:45:09 +0000

    Added Red_Gas_Station Model + Modern luxurious VILLA + One Building + Four Trash Cans Model + Replaced one building + Fixed some issue.

--- src/ModernCity/City.jsx
+++ src/ModernCity/City.jsx
@@ -15,7 +15,7 @@ import { Axes } from '../Components/AxesHelper';
 import "../../src/CompCSS/ThreeD.css"
 import { CityModel2 } from './Models/CityModel2';
 import { Market } from './Models/SuperMarket';
-
+import { Building_One } from './Models/Building_One';
 import { BasketBallStad } from './Models/basketBallStad';
 import { Disco } from './Models/Disco';
 import { McDonalds } from './Models/mcDonalds';
@@ -26,6 +26,9 @@ import { HyperCar2 } from './Models/CarHyper2';
 import { GreyTheater } from './Models/GreyTheater';
 import { CarPackSix } from './Models/CarPackSix';
 import { TheaterBig } from './Models/TheaterBig';
+import { Modern_VILLA } from './Models/Modern_VILLA';
+import { Trash } from './Models/Trash';
+import { Gas_Station } from './Models/Gas_Station';
 
 function City() {  
 
@@ -67,7 +70,7 @@ function City() {
         <Market position={[-7, -1.5, -157]} rotation={[0, Math.PI, 0]} scale={[4, 4, 4]}/>
         <BasketBallStad position={[-150, 1, -79]} rotation={[0, Math.PI, 0]} scale={[5, 5, 5]}/>
         <McDonalds position={[15, 0, -30]} rotation={[0, 0, 0]} scale={[1.3, 1.5, 1.5]}/>
-        <House position={[-90, -5, 30]} rotation={[0, 0, 0]} scale={[10,10, 10]}/>
+        <House position={[-160,-5,260]} rotation={[0, 0, 0]} scale={[10,10, 10]}/>
         <pointLight position={[32, 10, 30]} intensity={1000} color="#fff" target={new THREE.Vector3(0, 0, -1)} />
         <HyperCar1 position={[30,0.2, 30]} rotation={[0, 0, 0]} scale={[150,150, 150]}/>
         {/* <pointLight position={[35, 5, 30]} intensity={100} color="#fff" target={new THREE.Vector3(0, 0, -1)} /> */}
@@ -81,6 +84,11 @@ function City() {
        <GreyTheater position ={[25,0.4,190]} rotation={[0, 90, 0]} scale ={[0.8,0.8,0.8]} /> 
        <CarPackSix position ={[10,0.1,120]} rotation={[0, 0, 0]} scale ={[2.3,2.3,2.3]} />
        <TheaterBig position ={[15,2,350]} rotation={[0, 0, 0]} scale ={[1.4,1.4,1.4]} />
+       <Building_One position ={[-100,0.1,380]} rotation={[0,0,0]} scale ={[0.007,0.007,0.007]} />
+       <Modern_VILLA position ={[-70, -1, 50]} rotation={[0,359.7,0]} scale ={[2,2,2]} />
+        <pointLight position={[35,10,-20]} intensity={1000} color="#fff" target={new THREE.Vector3(0, 0, -1)} />
+        <Trash position ={[35, 0, -20]} rotation={[0,359.7,0]} scale ={[0.05,0.05,0.05]} />
+        <Gas_Station position={[-147,-0.4,200]} rotation={[0, 69.12, 0]} scale={[2,2.5, 2]}/>
         </Physics>
       <PointerLockControls />
       </Canvas>
